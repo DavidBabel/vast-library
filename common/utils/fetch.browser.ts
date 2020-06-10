6494aa8fc9667f52665678268fbe0e7deb9d873b
Julien Petit <julien.petit@ogury.co>
2020-06-10 17:23:24 +0200

    add error log messages to fetchUrlSync (#36)

--- common/utils/fetch.browser.ts
+++ common/utils/fetch.browser.ts
@@ -19,20 +19,28 @@ interface FetchUrlAsyncOptions {
 
 function fetchUrlSync({ url, retries = 2 }: FetchUrlSyncOptions) {
   let attempts = 0;
+  const errors = [];
 
   while (attempts++ < retries) {
+    let request;
     try {
-      const request = new XMLHttpRequest();
+      request = new XMLHttpRequest();
       request.open("GET", url, false);
       request.send();
 
       if (request.status >= 200 && request.status < 400) {
         return request.responseText;
       }
-    } catch (e) {}
+    } catch (e) {
+      if (request && request.status) {
+        errors.push(`REQ #${attempts} status : ${request.status}`);
+      }
+
+      errors.push(`REQ #${attempts} message : ${e.message}`);
+    }
   }
 
-  throw new Error(`${url} fetch failed after ${attempts} attempts`);
+  throw new Error(`${url} fetch failed after ${attempts} attempts. ${errors.join(', ')}`);
 }
 
 function fetchUrlAsync({ url, onError, onSuccess, retries = 2 }: FetchUrlAsyncOptions) {
